577726069910894bb28240da12c6eb2b4f758c08
Janek0337 <artyleriapolska@gmail.com>
2025-08-04 14:19:51 +0200

    fixed typo and prepared for future models

--- api.py
+++ api.py
@@ -9,11 +9,11 @@ app = FastAPI()
 class CertaintyDTO(BaseModel):
     certainty: float
 
-@app.post("/verify")
-async def verity_image(file: UploadFile = File(...), type: str = Form(...)):
-    certainty = await get_certainty(file, type)
+@app.post("/verify/image")
+async def verify_image(file: UploadFile = File(...), type: str = Form(...)):
+    certainty = await get_image_certainty(file, type)
     return {"certainty": certainty}
 
-async def get_certainty(file, type):
+async def get_image_certainty(file, type):
     # TODO / temporary placeholder return value
     return random.randrange(1,100)/100

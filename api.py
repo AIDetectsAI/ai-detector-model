3403e87b81b603d58e8487881b5fba2544d6c509
Janek0337 <artyleriapolska@gmail.com>
2025-08-10 18:45:45 +0200

    improved todo marking

--- api.py
+++ api.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI, File, UploadFile, Form
 from pydantic import BaseModel
 
-# for future removal
-import random
-
 app = FastAPI()
+
+class ModelController():
+    def __init__(self):
+        # TODO initialize model
+        print("Initializing...")
+
+    async def get_image_certainty(file: File, type: str) -> float:
+        # TODO call model for certainty
+        return 0.69
+
+model_handler = ModelController()
 
 class CertaintyDTO(BaseModel):
     certainty: float
 
 @app.post("/verify/image")
 async def verify_image(file: UploadFile = File(...), type: str = Form(...)):
-    certainty = await get_image_certainty(file, type)
+    certainty = await model_handler.get_image_certainty(file, type)
     return {"certainty": certainty}
-
-async def get_image_certainty(file, type):
-    # TODO / temporary placeholder return value
-    return random.randrange(1,100)/100

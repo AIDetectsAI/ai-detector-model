20d3bdd61622eb974d20669e7e2f68c45b2f934d
Janek0337 <artyleriapolska@gmail.com>
2025-09-24 17:54:48 +0200

    added integration test for whole api

--- ai_detector_model/model_converter.py
+++ ai_detector_model/model_converter.py
@@ -2,6 +2,8 @@ import torch
 import onnxruntime
 from ai_detector_model.config import *
 from models.pytorch.baseline_model_class import CustomBinaryCNN
+import numpy as np
+
 def convert_pth_to_onnx(model_class, pth_model_path: str, onnx_save_path: str):
     model = model_class()
     model.load_state_dict(torch.load(pth_model_path, map_location=torch.device('cpu')))
@@ -17,11 +19,11 @@ class ModelController():
         self.ort_session = onnxruntime.InferenceSession(onnx_model_path, providers=["CPUExecutionProvider"])
 
     def run_onnx_model(self, tensor_to_use):
-        onnx_input = tensor_to_use.detach().cpu().numpy()
+        onnx_input = tensor_to_use.astype(np.float32)
         input_name = self.ort_session.get_inputs()[0].name
         onnxruntime_input = {input_name : onnx_input}
         onnxruntime_output = self.ort_session.run(None, onnxruntime_input)[0]
-        return onnxruntime_output
+        return float(onnxruntime_output.item())
 
 if __name__ == "__main__":
     pth_path = "models/pytorch/baseline_model.pth"
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,9 @@
-import pytest
 from fastapi.testclient import TestClient
-from fastapi import File
 from ai_detector_model.config import *
 from ai_detector_model.api.api import APIController
 from ai_detector_model.api.api import app
+from PIL import Image
+import io
 
 def test_verify_image(monkeypatch):
 
@@ -20,3 +20,20 @@ def test_verify_image(monkeypatch):
 
     assert response.status_code == 200
     assert response.json()["certainty"] == 0.995
+
+def test_endpoint_logic():
+    img = Image.new("RGB", (64, 64), color=(255, 0, 0))
+    buf = io.BytesIO()
+    img.save(buf, format="PNG")
+    buf.seek(0)
+
+    client = TestClient(app)
+    files = {"file": ("test.png", buf, "image/png")}
+    data = {"type": "image"}
+    response = client.post("/verify/image", files=files, data=data)
+
+    assert response.status_code == 200
+    json_data = response.json()
+    assert "certainty" in json_data
+    assert isinstance(json_data["certainty"], (float, int))
+    assert 0 <= json_data["certainty"] <= 1

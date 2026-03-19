16a6753f436f53524dc183f0c2f332255031771e
jarzeckil <153275199+jarzeckil@users.noreply.github.com>
2026-03-20 00:48:21 +0100

    feat: start working on training script

--- ai_detector_model/modeling/train.py
+++ ai_detector_model/modeling/train.py
@@ -4,9 +4,12 @@ import hydra
 from loguru import logger
 from omegaconf import DictConfig
 from torch import cuda
-from tqdm import tqdm
+import torch.nn as nn
+from torchvision.models import EfficientNet
 
-from ai_detector_model.config import CONFIG_DIR
+from ai_detector_model.core.config import CONFIG_DIR
+from ai_detector_model.data.factory import create_loaders
+from ai_detector_model.modeling.engine import Trainer
 
 # DEVICE SETUP
 DEVICE = os.getenv("TRAIN_DEVICE")
@@ -31,11 +34,51 @@ logger.info(f"Training device: {DEVICE}")
 def main(
     cfg: DictConfig,
 ):
-    logger.info(f"epochs: {cfg.train.epochs}")
-    logger.info("Training some model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
+    logger.info("Creating data loaders")
+    train_loader, test_loader = create_loaders(cfg)
+
+    logger.info("Instantiating objects")
+
+    model = hydra.utils.instantiate(cfg.model.estimator)
+
+    if cfg.model.model_type == "efficientnet":
+        model: EfficientNet
+        model.classifier[1] = nn.Linear(
+            in_features=model.classifier[1].in_features,
+            out_features=cfg.data.num_classes,
+        )
+        backbone_params = [
+            param for name, param in model.named_parameters() if "classifier" not in name
+        ]
+        head_params = model.classifier.parameters()
+
+        optim_partial = hydra.utils.instantiate(cfg.train.optimizer.adamw)
+        base_lr = cfg.optimizer.adamw.lr
+
+    else:
+        raise ValueError("Model type unknown")
+
+    model = model.to(device=DEVICE)
+
+    loss_fn = hydra.utils.instantiate(cfg.train.loss_function)
+    optim = optim_partial(
+        [
+            {"params": backbone_params, "lr": base_lr * 0.1},
+            {"params": head_params},
+        ]
+    )
+
+    trainer = Trainer(
+        model=model,
+        loss_fn=loss_fn,
+        optim=optim,
+        train_loader=train_loader,
+        test_loader=test_loader,
+        device=DEVICE,
+    )
+    trainer.train()
+
+    logger.info("Starting model training")
     logger.success("Modeling training complete.")
 
 
